14f32ceb168f1809be9c65cacfb89292049a5c16
LeBi26 <leon.biermann@gmx.net>
2021-07-24 11:15:50 +0200

    made application.py smaller by putting some functions into helpers.py

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 # Query for routes like: Routes/Tasks/Add
 
 from cs50 import SQL
-from flask import Flask, url_for, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import login_required
+from helpers import login_required, createRequest, getChallenges, return_error, respondRequest, getTasks, getUsername, getRequests
 
 # Configure application
 app = Flask(__name__)
@@ -38,19 +38,6 @@ Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///tasks.db")
-
-
-def return_error(message):
-    return render_template("error.html", message=message)
-
-
-def getUsername(id):
-    try:
-        username = db.execute("SELECT username FROM users WHERE id = ?",
-                              id)[0]["username"]
-    except:
-        username = None
-    return username
 
 
 # Route/
@@ -183,11 +170,6 @@ def logout():
 
     # Redirect user to login form
     return redirect("/login")
-
-
-def getTasks():
-    return db.execute("SELECT * FROM tasks WHERE user_id=? AND fullfilled=0",
-                      session.get("user_id"))
 
 
 # Routes/Tasks
@@ -412,32 +394,6 @@ def profileEdit():
         return redirect("/profile")
 
 
-def getRequests(user_id, type):
-    incoming_requests = set()
-    requests = db.execute(
-        "SELECT * FROM requests WHERE recipient_id = ? AND type = ?", user_id,
-        type)
-    for req in requests:
-        try:
-            name = db.execute("SELECT username FROM users WHERE id = ?",
-                              req["sender_id"])[0]["username"]
-            incoming_requests.add(name)
-        except:
-            print("An error occured with one friend request.")
-    pending_requests = set()
-    requests = db.execute(
-        "SELECT * FROM requests WHERE sender_id = ? AND type = ?", user_id,
-        type)
-    for req in requests:
-        try:
-            name = db.execute("SELECT username FROM users WHERE id = ?",
-                              req["recipient_id"])[0]["username"]
-            pending_requests.add(name)
-        except:
-            print("An error occured with one friend request.")
-    return incoming_requests, pending_requests
-
-
 def getFriends():
     user_id = session.get("user_id")
     # Get the friends from the db
@@ -483,43 +439,6 @@ def friends():
                            friends=friends,
                            incoming_requests=incoming_requests,
                            pending_requests=pending_requests)
-
-
-def respondRequest(type, add):
-    user_id = session.get("user_id")
-    # Make sure that the request exists
-    sender = db.execute("SELECT id FROM users WHERE username = ? ",
-                        request.form.get("username"))
-    if len(sender) == 0:
-        return render_template("error.html",
-                               message="User not found. Please try again. ")
-    try:
-        sender_id = sender[0]["id"]
-    except:
-        return render_template(
-            "error.html",
-            message="Sender id was not unpacked correctly. Please try again. ")
-
-    req = db.execute(
-        "SELECT * FROM requests WHERE sender_id = ? AND recipient_id = ? AND type=?",
-        sender_id, user_id, type)
-    if len(req) == 0:
-        return render_template(
-            "error.html",
-            message="The request was not found. Please try again. ")
-    # Delete the open request and insert the friendship if needed
-    if (add):
-        if type == 0:
-            db.execute(
-                "INSERT INTO friends (first_user_id, second_user_id) VALUES (?,?)",
-                user_id, sender_id)
-        if type == 1:
-            db.execute(
-                "INSERT INTO challenges(challenger_id, challenged_id, challenger_score, challenged_score, finished, expire_date) VALUES (?, ?, 0, 0, false, Date('now', '+7 days'))",
-                sender_id, user_id)
-    db.execute(
-        "DELETE FROM requests WHERE sender_id = ? AND recipient_id = ? AND type=?",
-        sender_id, user_id, type)
 
 
 # Routes/Friends/Add/Accept
@@ -649,29 +568,6 @@ def friendsTeamUp():
     )
 
 
-def getChallenges(history):
-    challenges_set = set()
-    user_id = session.get("user_id")
-    if history:
-        challenges = db.execute(
-            "SELECT * FROM challenges WHERE (challenger_id = ? OR challenged_id = ?) AND expire_date < Date('now')",
-            user_id, user_id)
-    else:
-        challenges = db.execute(
-            "SELECT * FROM challenges WHERE (challenger_id = ? OR challenged_id = ?) AND expire_date >= Date('now')",
-            user_id, user_id)
-    for challenge in challenges:
-        first_user = db.execute("SELECT username FROM users WHERE id = ?",
-                                challenge["challenger_id"])[0]["username"]
-        second_user = db.execute("SELECT username FROM users WHERE id = ?",
-                                 challenge["challenged_id"])[0]["username"]
-        challenge = (challenge["id"], challenge["challenger_score"],
-                     challenge["challenged_score"], first_user, second_user,
-                     challenge["expire_date"])
-        challenges_set.add(challenge)
-    return challenges_set
-
-
 # Routes/Challenges
 @app.route("/challenges", methods=["GET"])
 @login_required
@@ -782,9 +678,3 @@ def challengesResults():
                 "error.html", message="An error occured. Please try again. ")
         createRequest(session.get("user_id"), user_id, 1)
         return redirect("/challenges")
-
-
-def createRequest(sender_id, recipient_id, type):
-    db.execute(
-        "INSERT INTO requests (sender_id, recipient_id, type) VALUES (?, ?, ?)",
-        sender_id, recipient_id, type)
--- helpers.py
+++ helpers.py
@@ -1,6 +1,125 @@
 from flask import redirect, session
 from functools import wraps
 
+from cs50 import SQL
+from flask import redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///tasks.db")
+
+
+def return_error(message):
+    return render_template("error.html", message=message)
+
+
+def getUsername(id):
+    try:
+        username = db.execute("SELECT username FROM users WHERE id = ?",
+                              id)[0]["username"]
+    except:
+        username = None
+    return username
+
+
+def getTasks():
+    return db.execute("SELECT * FROM tasks WHERE user_id=? AND fullfilled=0",
+                      session.get("user_id"))
+
+
+def getRequests(user_id, type):
+    incoming_requests = set()
+    requests = db.execute(
+        "SELECT * FROM requests WHERE recipient_id = ? AND type = ?", user_id,
+        type)
+    for req in requests:
+        try:
+            name = db.execute("SELECT username FROM users WHERE id = ?",
+                              req["sender_id"])[0]["username"]
+            incoming_requests.add(name)
+        except:
+            print("An error occured with one friend request.")
+    pending_requests = set()
+    requests = db.execute(
+        "SELECT * FROM requests WHERE sender_id = ? AND type = ?", user_id,
+        type)
+    for req in requests:
+        try:
+            name = db.execute("SELECT username FROM users WHERE id = ?",
+                              req["recipient_id"])[0]["username"]
+            pending_requests.add(name)
+        except:
+            print("An error occured with one friend request.")
+    return incoming_requests, pending_requests
+
+
+def respondRequest(type, add):
+    user_id = session.get("user_id")
+    # Make sure that the request exists
+    sender = db.execute("SELECT id FROM users WHERE username = ? ",
+                        request.form.get("username"))
+    if len(sender) == 0:
+        return render_template("error.html",
+                               message="User not found. Please try again. ")
+    try:
+        sender_id = sender[0]["id"]
+    except:
+        return render_template(
+            "error.html",
+            message="Sender id was not unpacked correctly. Please try again. ")
+
+    req = db.execute(
+        "SELECT * FROM requests WHERE sender_id = ? AND recipient_id = ? AND type=?",
+        sender_id, user_id, type)
+    if len(req) == 0:
+        return render_template(
+            "error.html",
+            message="The request was not found. Please try again. ")
+    # Delete the open request and insert the friendship if needed
+    if (add):
+        if type == 0:
+            db.execute(
+                "INSERT INTO friends (first_user_id, second_user_id) VALUES (?,?)",
+                user_id, sender_id)
+        if type == 1:
+            db.execute(
+                "INSERT INTO challenges(challenger_id, challenged_id, challenger_score, challenged_score, finished, expire_date) VALUES (?, ?, 0, 0, false, Date('now', '+7 days'))",
+                sender_id, user_id)
+    db.execute(
+        "DELETE FROM requests WHERE sender_id = ? AND recipient_id = ? AND type=?",
+        sender_id, user_id, type)
+
+
+def getChallenges(history):
+    challenges_set = set()
+    user_id = session.get("user_id")
+    if history:
+        challenges = db.execute(
+            "SELECT * FROM challenges WHERE (challenger_id = ? OR challenged_id = ?) AND expire_date < Date('now')",
+            user_id, user_id)
+    else:
+        challenges = db.execute(
+            "SELECT * FROM challenges WHERE (challenger_id = ? OR challenged_id = ?) AND expire_date >= Date('now')",
+            user_id, user_id)
+    for challenge in challenges:
+        first_user = db.execute("SELECT username FROM users WHERE id = ?",
+                                challenge["challenger_id"])[0]["username"]
+        second_user = db.execute("SELECT username FROM users WHERE id = ?",
+                                 challenge["challenged_id"])[0]["username"]
+        challenge = (challenge["id"], challenge["challenger_score"],
+                     challenge["challenged_score"], first_user, second_user,
+                     challenge["expire_date"])
+        challenges_set.add(challenge)
+    return challenges_set
+
+
+def createRequest(sender_id, recipient_id, type):
+    db.execute(
+        "INSERT INTO requests (sender_id, recipient_id, type) VALUES (?, ?, ?)",
+        sender_id, recipient_id, type)
+
 
 def login_required(f):
     """
